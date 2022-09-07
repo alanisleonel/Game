22ab98b6ef725ae74c5f5b2b21d13e46b86fb4e3
Leonel Alanis <l.alanis1989@gmail.com>
2022-09-06 19:41:52 -0500

    added my blocks

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //selecting the grid class
 const grid = document.querySelector('.grid')
+const blockHeight = 20
+const blockWidth = 100
+
 //creating a block
 class Block {
     constructor(xAxis, yAxis) {
@@ -9,9 +12,34 @@ class Block {
         this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
     }
 }
-//creating blocks
-const block = document.createElement('div')
-block.classList.add('block')
-block.style.left = '100px'
-block.style.bottom = '50px'
-grid.appendChild(block)
+
+
+const blocks = [
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210)
+]
+
+//adding blocks
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = blocks[i].bottomLeft[0] + 'px'
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+    grid.appendChild(block)
+    }
+}
+addBlocks()
